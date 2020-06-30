264cad232016d479ec2a623d1e5a9d293ef32418
Christian <christianrenovel83@gmail.com>
2020-06-30 15:47:09 +0100

    primer

---
+++ assets/js/CarouselTtle.js
@@ -0,0 +1,24 @@
+
+var titles=[
+    "uno",
+    "dos",
+    "tres"
+];
+
+var x = 0;  
+var botNext = document.getElementById('botPrev');
+
+document.getElementById("titleCar").innerHTML = titles[0];
+
+function Next(){
+  
+
+  
+   
+
+}
+
+function Prev(){
+
+}
+
